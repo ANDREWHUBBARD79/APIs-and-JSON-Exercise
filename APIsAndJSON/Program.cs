513579ebace5a82bdede124a21f6bfc5f8d6fce5
Andrew <andrewhubbard78@gmail.com>
2023-10-12 15:20:51 -0400

    A

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSon
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            
+            
+                QuoteGenerator.KanyeQuote();
+
+                QuoteGenerator.RonQuote();
+
+            
+
         }
+
+
     }
 }
